753946284b698a5fafbd0a375c057bb50f2fb136
ruifeng.kuang <ruifeng.kuang@vipshop.com>
2023-01-17 14:30:55 +0800

    用git archive优化：只获取需要的sdk文件

--- scripts/update-sdk/index.js
+++ scripts/update-sdk/index.js
@@ -1,12 +1,13 @@
 import { fileURLToPath } from 'node:url'
-import configs from '../../configs.js'
-import { execute, version2Date, deleteDir } from '../../utils/index.js'
 import fs from 'node:fs'
 import path from 'node:path'
 import { red, green, bold } from 'kolorist'
+import compressing  from 'compressing'
+import configs from '../../configs.js'
+import { execute, version2Date, deleteDir } from '../../utils/index.js'
 import renderTemplate from '../create-app/renderTemplate.js'
 
-export default function () {
+export default async function () {
   const { repo, branch, sdkPath: latestSdkPath, sdkVersionPath: latestVersionPath } = configs.vue3Portal
   // const { repo, branch, sdkPath: latestSdkPath, sdkVersionPath: latestVersionPath } = configs.vue3Template
   const { sdkPath: localSdkPath, sdkVersionPath: localVersionPath } = configs.vue3Template
@@ -20,9 +21,12 @@ export default function () {
     console.log(red(`读取本地sdk版本信息失败，请在通过vue3模板创建的工程根目录下执行命令`))
     process.exit(1)
   }
-  const tempSdkDir = fileURLToPath(new URL('./sdk', import.meta.url))
-  execute(`git clone ${repo} --branch=${branch} --depth=1 ${tempSdkDir}`)
+
+  const tempZipSdkDir = fileURLToPath(new URL('./latestSdk.zip', import.meta.url))
+  const tempSdkDir = fileURLToPath(new URL('./latestSdk', import.meta.url))
+  execute(`git archive -o ${tempZipSdkDir} --remote "${repo}" "${branch}" "${latestSdkPath}"`)
   try {
+    await compressing.zip.uncompress(tempZipSdkDir, tempSdkDir)
     latestSdkVersion = fs.readFileSync(path.join(tempSdkDir, latestVersionPath), 'utf8')
   } catch (e) {
     console.log(red(`获取git仓库中sdk版本信息失败`))
@@ -33,6 +37,7 @@ export default function () {
   const latestSdkVersionDate = version2Date(latestSdkVersion.trim())
   if (localSdkVersionDate.getTime() >= latestSdkVersionDate.getTime()) {
     console.log(green(`\n当前sdk已是最新版本`))
+    fs.unlinkSync(tempZipSdkDir)
     deleteDir(tempSdkDir)
     process.exit(1)
   } else {
@@ -42,8 +47,8 @@ export default function () {
       src: path.join(tempSdkDir, latestSdkPath),
       dest: path.join(cwd, localSdkPath)
     })
+    fs.unlinkSync(tempZipSdkDir)
     deleteDir(tempSdkDir)
-    console.log()
-    console.log(green(`sdk更新成功，更新后Sdk版本为${bold(latestSdkVersion)}`))
+    console.log(green(`\nsdk更新成功，更新后Sdk版本为${bold(latestSdkVersion)}`))
   }
 }
